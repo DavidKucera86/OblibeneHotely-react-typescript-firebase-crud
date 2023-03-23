79652e34c3ac827f6e21b3d81ae8cbf99bec4ce4
David Kučera <dvdkcr@seznam.cz>
2023-03-23 10:14:54 +0100

    loading data fromj firebase finished

---
+++ hotel-dashboard/src/components/Card.tsx
@@ -0,0 +1,44 @@
+import React, { useEffect, useState } from 'react'
+import { hotelsCollection } from '../lib/controller'
+import { DocumentData, QuerySnapshot, onSnapshot } from 'firebase/firestore'
+import { NewHotelType } from '../types/hotel'
+import Information from './Information'
+
+const Card = () => {
+
+  const [hotels, setHotels] = useState<NewHotelType[]>([])
+
+  useEffect(() => onSnapshot(hotelsCollection, (snapshot: QuerySnapshot<DocumentData>) => {
+    setHotels(
+      snapshot.docs.map((doc) => {
+        return {
+          id: doc.id,
+          ...doc.data(),
+
+        }
+
+      })
+    )
+
+  }), [])
+  console.log(hotels, "hotels");
+
+  return (
+    <div className='card'>
+      <h2 className="title">All hotels</h2>
+      {hotels && hotels.length ? (
+        <div>
+          {
+            hotels?.map((hotel) => (
+              <Information key={hotel.id} hotel={hotel} />
+            ))
+          }
+        </div>
+      ) : (
+        <h2 className="no-hotels">There are no hotels. Please add one.</h2>
+      )}
+    </div>
+  )
+}
+
+export default Card
---
+++ hotel-dashboard/src/lib/firebase.ts
@@ -0,0 +1,14 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyC2KidflRoIpYcUPy90iKOyl8Rx-Wlapdg",
+  authDomain: "hotel-dashboard-9f1cb.firebaseapp.com",
+  projectId: "hotel-dashboard-9f1cb",
+  storageBucket: "hotel-dashboard-9f1cb.appspot.com",
+  messagingSenderId: "1008526421746",
+  appId: "1:1008526421746:web:2b05cd02bf437f562d6a6b",
+  measurementId: "G-LBMW9N3GTL"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
